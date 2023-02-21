118948f34c5d0427bee0737c4c1d4be1e3396f04
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-21 10:40:39 +0200

    feature: added functional of search

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './template.html';
 import './assets/styles/index.scss';
+
+const input = document.querySelector('.search__input');
+const products = document.querySelectorAll('.search__product');
+
+const onInput = (e) => {
+  let inputData = e.target.value.toLowerCase().trim();
+
+  products.forEach((product) => {
+    let name = product
+      .querySelector('.search__product-name')
+      .innerHTML.toLowerCase()
+      .trim();
+
+    console.log(name);
+    if (!name.includes(inputData)) {
+      product.style.display = 'none';
+    } else {
+      product.style.display = 'flex';
+    }
+  });
+};
+
+input.addEventListener('input', onInput);
